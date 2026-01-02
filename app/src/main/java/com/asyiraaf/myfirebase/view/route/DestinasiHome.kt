6d7956636fb51b6838fb34686081150719768bff
asyiraafnufail <asyiraaf4@gmail.com>
2026-01-02 16:37:42 +0700

    menambahkan objek baru

---
+++ app/src/main/java/com/asyiraaf/myfirebase/view/route/DestinasiHome.kt
@@ -0,0 +1,9 @@
+package com.asyiraaf.myfirebase.view.navigasi
+
+import com.asyiraaf.myfirebase.view.route.DestinasiNavigasi
+import com.example.myfirebase.R
+
+object DestinasiHome : DestinasiNavigasi {
+    override val route = "home"
+    override val titleRes = R.string.app_name
+}
