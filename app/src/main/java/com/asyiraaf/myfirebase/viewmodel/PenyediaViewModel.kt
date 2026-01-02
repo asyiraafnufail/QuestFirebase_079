7a162b870c80a51446cd48ec41cd6ace98557fdf
asyiraafnufail <asyiraaf4@gmail.com>
2026-01-02 18:45:17 +0700

    membuat viewmodel

---
+++ app/src/main/java/com/asyiraaf/myfirebase/viewmodel/PenyediaViewModel.kt
@@ -0,0 +1,29 @@
+package com.asyiraaf.myfirebase.viewmodel
+
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.viewmodel.CreationExtras
+import androidx.lifecycle.viewmodel.initializer
+import androidx.lifecycle.viewmodel.viewModelFactory
+import com.asyiraaf.myfirebase.repositori.AplikasiDataSiswa
+
+object PenyediaViewModel {
+    val Factory = viewModelFactory {
+
+        // Initializer untuk HomeViewModel
+        initializer {
+            HomeViewModel(aplikasiDataSiswa().container.repositorySiswa)
+        }
+
+        // Initializer untuk EntryViewModel
+        initializer {
+            EntryViewModel(aplikasiDataSiswa().container.repositorySiswa)
+        }
+    }
+}
+
+/**
+ * Fungsi ekstensi untuk mengambil objek AplikasiDataSiswa dari CreationExtras.
+ * Pastikan nama class 'AplikasiDataSiswa' sesuai dengan yang ada di ContainerApp.kt
+ */
+fun CreationExtras.aplikasiDataSiswa(): AplikasiDataSiswa =
+    (this[ViewModelProvider.AndroidViewModelFactory.APPLICATION_KEY] as AplikasiDataSiswa)
