2ef98b0d1794454a409c7627398e3f8dcb2489fa
asyiraafnufail <asyiraaf4@gmail.com>
2026-01-02 16:22:49 +0700

    Menambahkan class container

--- app/src/main/java/com/asyiraaf/myfirebase/repositori/ContainerApp.kt
+++ app/src/main/java/com/asyiraaf/myfirebase/repositori/ContainerApp.kt
@@ -6,3 +6,16 @@ interface ContainerApp {
     val repositorySiswa: RepositorySiswa
 }
 
+class DefaultContainerApp : ContainerApp {
+    override val repositorySiswa: RepositorySiswa by lazy {
+        FirebaseRepositorySiswa()
+    }
+}
+
+class AplikasiDataSiswa : Application() {
+    lateinit var container: ContainerApp
+    override fun onCreate() {
+        super.onCreate()
+        this.container = DefaultContainerApp()
+    }
+}
