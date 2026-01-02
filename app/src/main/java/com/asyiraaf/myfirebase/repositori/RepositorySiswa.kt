ae47bbdf2c109150aea766763d159bff4545fc83
asyiraafnufail <asyiraaf4@gmail.com>
2026-01-02 16:23:56 +0700

    Membuat interface RepositorySiswa untuk manajemen data siswa

--- app/src/main/java/com/asyiraaf/myfirebase/repositori/RepositorySiswa.kt
+++ app/src/main/java/com/asyiraaf/myfirebase/repositori/RepositorySiswa.kt
@@ -1,2 +1,11 @@
 package com.asyiraaf.myfirebase.repositori
 
+import com.asyiraaf.myfirebase.modeldata.Siswa
+import com.google.firebase.firestore.FirebaseFirestore
+import kotlinx.coroutines.tasks.await
+
+interface RepositorySiswa {
+    suspend fun getDataSiswa(): List<Siswa>
+    suspend fun postDataSiswa(siswa: Siswa)
+}
+
