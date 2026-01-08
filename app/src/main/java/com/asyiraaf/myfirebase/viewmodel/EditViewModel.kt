e2f6d56b227b1f0a5bedcb2cbc7ed3c0971fcfe4
asyiraafnufail <asyiraaf4@gmail.com>
2026-01-08 11:04:52 +0700

    menambahkan class edit view model

--- app/src/main/java/com/asyiraaf/myfirebase/viewmodel/EditViewModel.kt
+++ app/src/main/java/com/asyiraaf/myfirebase/viewmodel/EditViewModel.kt
@@ -1,2 +1,9 @@
 package com.asyiraaf.myfirebase.viewmodel
 
+import androidx.lifecycle.SavedStateHandle
+import com.asyiraaf.myfirebase.repositori.RepositorySiswa
+
+class EditViewModel(
+    savedStateHandle: SavedStateHandle,
+    private val repositorySiswa: RepositorySiswa
+)
