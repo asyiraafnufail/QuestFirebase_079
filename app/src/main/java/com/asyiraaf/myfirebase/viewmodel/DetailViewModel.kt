7d3048b9cc531e59d7951767e20b6c7bf1696aca
asyiraafnufail <asyiraaf4@gmail.com>
2026-01-08 11:12:19 +0700

    fix: stabilize UI detail status

--- app/src/main/java/com/asyiraaf/myfirebase/viewmodel/DetailViewModel.kt
+++ app/src/main/java/com/asyiraaf/myfirebase/viewmodel/DetailViewModel.kt
@@ -1,2 +1,9 @@
 package com.asyiraaf.myfirebase.viewmodel
 
+import com.asyiraaf.myfirebase.modeldata.Siswa
+
+sealed interface StatusUIDetail {
+    data class Success(val satusiswa: Siswa?) : StatusUIDetail
+    object Error : StatusUIDetail
+    object Loading : StatusUIDetail
+}
