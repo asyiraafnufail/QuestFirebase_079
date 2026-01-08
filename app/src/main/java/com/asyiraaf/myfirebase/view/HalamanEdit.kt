e7811ac02a66ee9f394f06d6d180663722f856e1
asyiraafnufail <asyiraaf4@gmail.com>
2026-01-08 10:59:27 +0700

    membuat desain halaman edit

--- app/src/main/java/com/asyiraaf/myfirebase/view/HalamanEdit.kt
+++ app/src/main/java/com/asyiraaf/myfirebase/view/HalamanEdit.kt
@@ -1,13 +1,16 @@
 package com.asyiraaf.myfirebase.view
 
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.asyiraaf.myfirebase.view.controllNavigasi.DestinasiEdit
 import com.asyiraaf.myfirebase.viewmodel.PenyediaViewModel
+import kotlinx.coroutines.launch
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -26,7 +29,18 @@ fun EditSiswaScreen(
             )
         },
         modifier = modifier
-    ){
-
+    ) { innerPadding ->
+        val coroutineScope = rememberCoroutineScope()
+        EntrySiswaBody(
+            uiStateSiswa = viewModel.uiStateSiswa,
+            onSiswaValueChange = viewModel::updateUiState,
+            onSaveClick = {
+                coroutineScope.launch {
+                    viewModel.editSatuSiswa()
+                    navigateBack()
+                }
+            },
+            modifier = Modifier.padding(innerPadding)
+        )
     }
 }
