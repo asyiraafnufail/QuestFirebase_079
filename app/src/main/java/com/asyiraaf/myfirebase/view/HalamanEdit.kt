13b9cbbc7f0fcce1fcfefc0fbb522a97f81a0743
asyiraafnufail <asyiraaf4@gmail.com>
2026-01-08 10:57:37 +0700

    membuat screen edit

--- app/src/main/java/com/asyiraaf/myfirebase/view/HalamanEdit.kt
+++ app/src/main/java/com/asyiraaf/myfirebase/view/HalamanEdit.kt
@@ -1,2 +1,18 @@
 package com.asyiraaf.myfirebase.view
 
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.asyiraaf.myfirebase.viewmodel.PenyediaViewModel
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun EditSiswaScreen(
+    navigateBack: () -> Unit,
+    onNavigateUp: () -> Unit,
+    modifier: Modifier = Modifier,
+    viewModel: EditViewModel = viewModel(factory = PenyediaViewModel.Factory)
+) {
+
+}
