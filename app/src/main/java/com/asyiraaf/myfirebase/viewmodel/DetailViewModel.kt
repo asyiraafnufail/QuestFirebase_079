8a1d31a1ed9c16b5790406915b887291dc084577
asyiraafnufail <asyiraaf4@gmail.com>
2026-01-08 11:13:07 +0700

    fix: stabilize DetailViewModel dependencies

--- app/src/main/java/com/asyiraaf/myfirebase/viewmodel/DetailViewModel.kt
+++ app/src/main/java/com/asyiraaf/myfirebase/viewmodel/DetailViewModel.kt
@@ -1,9 +1,20 @@
 package com.asyiraaf.myfirebase.viewmodel
 
+import android.view.View
+import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.ViewModel
 import com.asyiraaf.myfirebase.modeldata.Siswa
+import com.asyiraaf.myfirebase.repositori.RepositorySiswa
 
 sealed interface StatusUIDetail {
     data class Success(val satusiswa: Siswa?) : StatusUIDetail
     object Error : StatusUIDetail
     object Loading : StatusUIDetail
+}
+
+class DetailViewModel(
+    savedStateHandle: SavedStateHandle,
+    private val repositorySiswa: RepositorySiswa
+) : ViewModel() {
+
 }
