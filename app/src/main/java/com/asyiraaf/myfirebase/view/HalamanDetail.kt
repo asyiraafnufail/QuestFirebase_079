0e2f733babefea1bc9f61dd1cf2f8604b193d6a5
asyiraafnufail <asyiraaf4@gmail.com>
2026-01-08 11:20:20 +0700

    update tampilan detail siswa

--- app/src/main/java/com/asyiraaf/myfirebase/view/HalamanDetail.kt
+++ app/src/main/java/com/asyiraaf/myfirebase/view/HalamanDetail.kt
@@ -1,2 +1,19 @@
 package com.asyiraaf.myfirebase.view
 
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.asyiraaf.myfirebase.viewmodel.DetailViewModel
+import com.asyiraaf.myfirebase.viewmodel.PenyediaViewModel
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun DetailSiswaScreen(
+    navigateToEditItem: (Long) -> Unit,
+    navigateBack: () -> Unit,
+    modifier: Modifier = Modifier,
+    viewModel: DetailViewModel = viewModel(factory = PenyediaViewModel.Factory)
+) {
+
+}
